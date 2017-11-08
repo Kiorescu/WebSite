e0dd6b12130b9f8c3d72bb7370ab39a3596913c2
Kiorescu <31818154+Kiorescu@users.noreply.github.com>
2017-11-08 18:13:40 +0200

    fara i

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-//schimbare
-//schimbare2
+//schmbare
+//schmbare2
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
